14380740d32315b32ebb12b031cf766f38ef35a2
Marozva <isabokefrankline@gmail.com>
2023-11-02 21:00:46 +0300

    completed

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function Header({name}) {
+    return (
+        <div>
+            <header>
+                <h1>{name}</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Header
